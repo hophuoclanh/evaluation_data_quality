af86a77641d0e63cb28505cc28ba6b344d934711
CaoDien2003 <118111366+CaoDien2003@users.noreply.github.com>
2023-10-08 12:29:55 +0700

    Update Class_Overlap and Class_Parity .py

--- Class_Overlap and Class_Parity .py
+++ Class_Overlap and Class_Parity .py
@@ -1,13 +1,5 @@
-import pandas as pd
 import numpy as np
 from imblearn.under_sampling import TomekLinks
-
-data = pd.read_csv('DATA.csv')
-df = data.drop(columns='STUDENT ID')
-
-# select data example 'GRADE'
-X = df.drop(columns='GRADE')
-y = df['GRADE']
 
 def Class_Overlap(x,y):
     tl = TomekLinks()
@@ -17,9 +9,6 @@ def Class_Overlap(x,y):
     DoQ_CO = 1 - (len(di) / total_data_samples)
     return DoQ_CO
 
-DoQ_CO =Class_Overlap(X, y)
-print("Overlap Regions (QoD_CD):", DoQ_CO)
-
 def Class_Parity(Ci):
     Ni = Ci.values
     ni = Ni / np.max(Ni)
@@ -27,5 +16,19 @@ def Class_Parity(Ci):
     QoD_CP = 1 - np.sum(np.abs(ni - n_mean)) / len(Ni)
 
     return QoD_CP
+
+# example DATA https://www.kaggle.com/datasets/joebeachcapital/students-performance?fbclid=IwAR2ZJZALZrT0wupHkhc3vQZwtAwajcTkbRSFla3mt92_ggYPS0YGNUjldoE
+import pandas as pd
+
+data = pd.read_csv('DATA.csv')
+df = data.drop(columns='STUDENT ID')
+
+# select data example 'GRADE'
+X = df.drop(columns='GRADE')
+y = df['GRADE']
+
+DoQ_CO =Class_Overlap(X, y)
+print("Overlap Regions (QoD_CD):", DoQ_CO)
+
 QoD_CP =   Class_Parity(y)
 print('Class Parity =', QoD_CP)
