52cb7ee888a24e2a7d899599463f136726dae3cf
CaoDien2003 <118111366+CaoDien2003@users.noreply.github.com>
2023-10-17 08:21:51 +0700

    Update Class_Overlap and Class_Parity .py

--- Class_Overlap and Class_Parity .py
+++ Class_Overlap and Class_Parity .py
@@ -1,5 +1,23 @@
+import pandas as pd
 import numpy as np
 from imblearn.under_sampling import TomekLinks
+from sklearn.neighbors import NearestNeighbors
+
+tl = TomekLinks
+
+data = pd.read_csv('DATA.csv')
+df = data.drop(columns='STUDENT ID')
+x = df.drop(columns=['COURSE ID', 'GRADE']).values
+y = df['GRADE'].values
+
+nn = NearestNeighbors(n_neighbors=30, algorithm='auto').fit(x)
+nn_distances, nn_indices = nn.kneighbors(x)
+print(type(y))
+print(type(nn_indices))
+
+di = tl.is_tomek(y, nn_indices, 0)
+
+
 
 def Class_Overlap(x,y):
     tl = TomekLinks()
@@ -14,21 +32,20 @@ def Class_Parity(Ci):
     ni = Ni / np.max(Ni)
     n_mean = np.mean(ni)
     QoD_CP = 1 - np.sum(np.abs(ni - n_mean)) / len(Ni)
-
     return QoD_CP
 
 # example DATA https://www.kaggle.com/datasets/joebeachcapital/students-performance?fbclid=IwAR2ZJZALZrT0wupHkhc3vQZwtAwajcTkbRSFla3mt92_ggYPS0YGNUjldoE
-import pandas as pd
 
 data = pd.read_csv('DATA.csv')
 df = data.drop(columns='STUDENT ID')
 
 # select data example 'GRADE'
+
 X = df.drop(columns='GRADE')
 y = df['GRADE']
 
-DoQ_CO =Class_Overlap(X, y)
-print("Overlap Regions (QoD_CD):", DoQ_CO)
+# DoQ_CO =Class_Overlap(X, y)
+# print("Overlap Regions (QoD_CD):", DoQ_CO)
 
-QoD_CP =   Class_Parity(y)
-print('Class Parity =', QoD_CP)
+# QoD_CP =   Class_Parity(y)
+# print('Class Parity =', QoD_CP)
